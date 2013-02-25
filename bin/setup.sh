6ca881a227adf96395e42c9b186d297bd00680b9
Fabian Groffen <fabian.groffen@booking.com>
2013-02-25 13:53:10 +0100

    setup: add some docu for the eager

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,6 +4,20 @@
 # Call scripts from here that you want to be executed after the
 # .dotfiles repository has been setup/updated
 
+# You can change this script in any way you want, including turning it
+# into some other script, such as Perl, Python or Ruby.  Note that
+# you're completely on your own here.
+
+# This script is called by /usr/local/bin/dotfiles.pl after git pull or
+# git clone with the following flags:
+#  -v  verbose: generate informative (debug) messages
+#  -f  force: override safety latches, mainly meant for the linkage of
+#      files in your homedir
+
+# Everything below is part of the standard setup recommended.  You are
+# advised to tune the defaults through your office page instead of
+# editing the script below.
+
 VERBOSE=
 [[ " $* " == *" -v "* ]] && VERBOSE=yes
 FORCE=
